8689c865e8d5b1ce4104622fc860cd61c3d7f099
Oritro Ahmed <oritromax@gmail.com>
2025-08-12 09:43:53 +0600

    Remapping Astro and Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,15 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    server: {
+      fs: {
+        allow: ['..']
+      }
+    },
+    optimizeDeps: {
+      exclude: ['astro:content']
+    }
   },
   site: 'https://ioritro.com',
   integrations: [sitemap()]
